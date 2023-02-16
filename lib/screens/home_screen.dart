0ad3a0a1b688bc384c7a7bc63ddd5aa9879adfe9
alexqgon93 <alexqgon93@gmail.com>
2023-02-16 15:34:50 +0100

    Added new widget and added to home screen below slider

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,16 +5,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Peliculas en cines'),
-        elevation: 0,
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined))
-        ],
-      ),
-      body: Column(
-        children: [CardSwiper()],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Peliculas en cines'),
+          elevation: 0,
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.search_outlined))
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Principal cards
+              CardSwiper(),
+              // Movie Slider
+              MovieSlider(),
+            ],
+          ),
+        ));
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,1 +1,2 @@
+export 'package:film_app_flutter/widgets/movie_slider.dart';
 export 'package:film_app_flutter/widgets/card_swiper.dart';
