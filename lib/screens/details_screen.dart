a5c558acf0ec08d549ef971b73d78e7ecb6a6590
alexqgon93 <alexqgon93@gmail.com>
2023-02-20 10:12:58 +0100

    Added new custom widgets on details screen, casting_cards and widgets exports

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,11 +1,110 @@
+import 'package:film_app_flutter/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('DetailsScreen'),
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const _CustomAppBar(),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            _PosterAndTitle(),
+            _OverView(),
+            _OverView(),
+            CastingCards(),
+            // casting de la Pelicula
+          ])),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      floating: false,
+      expandedHeight: 200,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+            color: Colors.black12,
+            alignment: Alignment.bottomCenter,
+            padding: const EdgeInsets.only(bottom: 5),
+            width: double.infinity,
+            child: const Text('movie.title', style: TextStyle(fontSize: 16))),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('movie.title',
+                  style: textTheme.headlineSmall,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2),
+              Text('movie.originalTitle',
+                  style: textTheme.titleMedium,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2),
+              Row(
+                children: [
+                  const Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                  Text('movie.voteAverage', style: textTheme.bodySmall)
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _OverView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        'Eiusmod non aliquip irure anim magna nulla quis. Sint anim non culpa qui eu ad ex. Cillum ea aute ex veniam occaecat esse anim cillum nulla cillum ut consequat officia laboris. Laborum sunt proident do exercitation ipsum non non sunt ut aliqua et. Nulla qui ex in do ullamco amet.',
+        textAlign: TextAlign.justify,
+        style: Theme.of(context).textTheme.titleMedium,
       ),
     );
   }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,3 @@
-export 'package:film_app_flutter/widgets/movie_slider.dart';
 export 'package:film_app_flutter/widgets/card_swiper.dart';
+export 'package:film_app_flutter/widgets/casting_cards.dart';
+export 'package:film_app_flutter/widgets/movie_slider.dart';
